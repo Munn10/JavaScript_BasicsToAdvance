a3040b2707e033ff94369d84ab914f41620e9c4a
Munna10 <muneer.king23@gmail.com>
2023-09-16 10:35:09 +0530

    updated code

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,39 @@
+let score = 33;
+// console.log(score);
+// console.log(typeof score);
+
+// With the help of typeof method we can know the data type of an variable.
+
+// To convert string to number we user Number() methos
+
+let convertedNumber = Number(score);
+// console.log(typeof convertedNumber);
+
+// console.log(typeof convertedNumber);
+// console.log(convertedNumber);
+
+// "33" => 33
+// "33admns" => NaN (Not a Number) it will converts to number but when we print the value it gives NaN
+// null => 0
+// undefined => NaN
+// true =>1 ; false => 0
+
+// Boolean conversion
+
+let isLoggedin = "Muneer";
+let booleanIsloggedin = Boolean(isLoggedin);
+// console.log(booleanIsloggedin);
+
+// If we pass:
+// 1 => true;
+// 0 => false;
+// "" => false;
+// "Munna" => true;
+
+// Convert to String:
+
+let sumNumber = 10;
+console.log(typeof sumNumber);
+let converted = String(sumNumber);
+console.log(typeof converted);
+console.log(converted);
