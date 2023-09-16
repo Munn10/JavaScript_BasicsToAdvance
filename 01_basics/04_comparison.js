5aee9ffad3ab0648c5e40228c6f6c521fcc131aa
Munna10 <muneer.king23@gmail.com>
2023-09-16 11:20:03 +0530

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 != 1);
+
+// console.log(undefined > 0); //false
+// console.log(undefined < 0); //false
+// console.log(undefined > !0); //false
+// console.log(undefined != 0); //true
+
+// console.log(undefined == null); //true
+// console.log(undefined > null); //false
+// console.log(undefined < null); //false
+// console.log(undefined >= null); //false
+// console.log(undefined <= null); //false
+// console.log(undefined != null); //false
+
+// == vs === (conditional check)
+// == check the value
+// === check the value and data type as well
+
+console.log("2" == 2); // true
+console.log("2" === 2); // false
