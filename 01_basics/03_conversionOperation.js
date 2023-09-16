2ca4685bf95498df94f2e87abe30f56c6d7c3069
Munna10 <muneer.king23@gmail.com>
2023-09-16 11:03:18 +0530

    conversionOperations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -33,7 +33,42 @@ let booleanIsloggedin = Boolean(isLoggedin);
 // Convert to String:
 
 let sumNumber = 10;
-console.log(typeof sumNumber);
+// console.log(typeof sumNumber);
 let converted = String(sumNumber);
-console.log(typeof converted);
-console.log(converted);
+// console.log(typeof converted);
+// console.log(converted);
+
+// ****************** OPERATIONS *********************** //
+let myNum = 26;
+let negative = -myNum;
+// console.log(negative);
+
+// Basic operations:
+// console.log(2+2); ==> addition
+// console.log(2-2); ==> subtraction
+// console.log(2*2); ==> multipy
+// console.log(2/2); ==> devide
+// console.log(2%2); ==> modulo to find remainders
+
+// tricy operations:
+
+// console.log(1 + "2");
+// console.log("1" + 5);
+// console.log(typeof ("1" + "8"));
+
+// Here when we add a number with string it concat it gives output as 12,15,18 in string format
+
+// console.log(1 + 3 + "5"); ==> 45
+// console.log(1 + "3" + 5); ==> 135
+// console.log("1" + 6 + 5); ==> 165
+
+// console.log(((3 + 3) * 6) / 5);
+
+// let a = 3;
+// let b = a++; ==> in postfix value will be incremented
+
+// console.log(a, b);
+
+let x = 4;
+let y = ++x;
+// console.log(x, y); ==> in prefix bothe values will be incremeted
