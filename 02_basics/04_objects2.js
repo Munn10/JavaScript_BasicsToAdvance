ba52f4c089d4589151c774413f9a6c4f5b6d06ae
Munna10 <muneer.king23@gmail.com>
2023-09-17 12:52:36 +0530

    json

--- 02_basics/04_objects2.js
+++ 02_basics/04_objects2.js
@@ -42,10 +42,48 @@ const newObj = { ...obj1, ...obj2, ...obj3, ...obj4 };
 
 // console.log(newObj);
 
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty("age"));
+// console.log(tinderUser.hasOwnProperty("age"));
+
+//// +++++++++++++++ Object Destructuring +++++++++++++++++++++
+
+const course = {
+  courseName: "Js in Hindi",
+  courseFee: "999",
+  courseInstructor: "Munna",
+};
+
+// console.log(course.courseInstructor); // This is noraml way of printing. Insteas we can destrure the things.
+
+// const { courseInstructor } = course; // we can change the key name as well
+// const { courseInstructor: instructor } = course;
+// console.log(instructor);
+
+const { courseName, courseFee, courseInstructor } = course; // this is called destructuring
+
+// Mostly we use this method in ReactJs
+
+// useually when we call apis we will get the data in the form of JSON
+
+// {
+//     "course":"JS in Hondi",
+//     "turor":"Munna",
+//     "courseFee":"free"
+// }
+
+// This is the JSON
+
+// we will get the data in some time in object and some in array of objects;
+
+// {
+//   [{}, {}, {}];
+// }
+
+// {
+//     {},{},{}
+// }
