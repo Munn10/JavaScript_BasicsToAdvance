4619a33c8f3b43525ccabf5dfb7fab82302bf53a
Munna10 <muneer.king23@gmail.com>
2023-09-21 11:07:25 +0530

    do while loop

---
+++ 05-iterations/02_do-while-loop.js
@@ -0,0 +1,29 @@
+// While loop:
+
+// while (condition) {
+
+// }
+
+let i = 0;
+
+// while (i <= 10) {
+//   console.log(`Value of i is ${i}`);
+//   //   i++;
+//   i = i + 2;
+// }
+
+const myArray = ["flash", "thor", "batman", "Super Man"];
+
+let arr = 0;
+while (arr < myArray.length) {
+  //   console.log(`Value of index ${arr} : ${myArray[arr]}`);
+  arr = arr + 1;
+  //   arr++;
+}
+
+// Do While Loop:
+let score = 0;
+do {
+  console.log(`Score is ${score}`);
+  score++;
+} while (score <= 10);
